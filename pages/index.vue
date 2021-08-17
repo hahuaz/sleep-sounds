6cb0325477fc082acf3b2326bc0f791169dd31ab
hahuaz <hahuaz@gmail.com>
2021-08-17 11:25:30 +0300

    add animation dynamically

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <p>sounds</p>
-    <p @click="pauseSound">pause</p>
+    <p @click="stopSound">stop sound</p>
     <div>
-      <div v-for="(sound, i) in audios" :key="i" @click="playSound(sound.src)">
+      <div
+        v-for="(sound, i) in audios"
+        :key="i"
+        :ref="sound.title"
+        @click="playSound(sound.src)"
+      >
         <p>{{ sound.title }}</p>
       </div>
     </div>
@@ -13,86 +18,85 @@
     </audio> -->
 
     <div class="anime__container">
-      <lottie
-        :options="defaultOptions"
-        :height="400"
-        :width="400"
-        @animCreated="handleAnimation"
-      />
-      <div>
-        <p>Speed: x{{ animationSpeed }}</p>
-        <input
-          v-model="animationSpeed"
-          type="range"
-          value="1"
-          min="0"
-          max="3"
-          step="0.5"
-          @change="onSpeedChange"
-        />
-      </div>
       <button @click="stop">stop</button>
       <button @click="pause">pause</button>
       <button @click="play">play</button>
+      <button style="width: 100px; height: 100px" @click="addAnimation">
+        add animation
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable-next-line */
+import lottie from 'lottie-web'
+
 import animations from '~/assets/animations.js'
+import ocean from '~/assets/audio/ocean.mp3'
+import oceanWavesWithRain from '~/assets/audio/ocean-waves-with-rain.mp3'
+import smallOceanWaves from '~/assets/audio/small-ocean-waves.mp3'
+
 export default {
   data() {
     return {
       currentSound: null,
-      isPlaying: null,
+      isPlaying: false,
       // audiosController: {
       //   ocean: require('@/assets/audio/ocean.mp3').default,
       // },
       audios: [
         {
           title: 'Heavy Ocean Waves',
-          src: new Audio(require('@/assets/audio/ocean.mp3').default),
+          src: new Audio(ocean),
         },
         {
           title: 'Ocean Waves With Rain',
-          src: new Audio(
-            require('@/assets/audio/ocean-waves-with-rain.mp3').default
-          ),
+          src: new Audio(oceanWavesWithRain),
         },
         {
           title: 'Small Ocean Waves',
-          src: new Audio(
-            require('@/assets/audio/small-ocean-waves.mp3').default
-          ),
+          src: new Audio(smallOceanWaves),
         },
       ],
-      defaultOptions: { animationData: animations.animetion1 },
-      animationSpeed: 1,
     }
   },
   beforeDestroy() {
     this.currentSound.pause()
   },
+  mounted() {
+    console.log(this.$refs)
+  },
 
   methods: {
     async playSound(sound) {
       try {
+        /* if there is a sound playing stop it first. */
+        if (this.currentSound) {
+          this.currentSound.pause()
+        }
         this.currentSound = sound
+        this.isPlaying = true
         this.currentSound.setAttribute('loop', 'true')
         await this.currentSound.play()
       } catch (error) {
         console.log(error)
       }
     },
-    pauseSound() {
+    stopSound() {
       this.currentSound.pause()
     },
 
     /* animation handlers */
-    handleAnimation(anim) {
-      this.anim = anim
+    addAnimation(e) {
+      this.anim = lottie.loadAnimation({
+        container: e.target,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: animations.playing,
+      })
     },
-
     stop() {
       this.anim.stop()
     },
@@ -105,9 +109,7 @@ export default {
       this.anim.pause()
     },
 
-    onSpeedChange() {
-      this.anim.setSpeed(this.animationSpeed)
-    },
+    /* add animation programaticly */
   },
 }
 </script>
